30b360e5f292ad671892ed548866206c7801d052
Laimonas_Ciurlionis <laimonas.ciurlionis@gmail.com>
2022-09-14 08:26:39 +0300

    Topic 2 and Topic 4 projects added

---
+++ Topic_2_Varaibles_Logic_Operators/Program.cs
@@ -0,0 +1,76 @@
+﻿Console.WriteLine("Enter a number: ");
+
+string userInput = Console.ReadLine();
+var x = Convert.ToInt32(userInput);
+var x1 = int.Parse(userInput);
+
+//kopijuoti: ctrl + d
+
+//string interpolation
+Console.WriteLine("{0}{0}{0}", x);
+Console.WriteLine("{0}  {0}", x);
+Console.WriteLine("{0}  {0}", x);
+Console.WriteLine("{0}  {0}", x);
+Console.WriteLine("{0}{0}{0}", x);
+
+string name = "Vardas";
+string lastName = "Pavarde";
+
+//Old version
+Console.WriteLine("My name is {0} and my lastname is {1}", name, lastName);
+
+//New version
+Console.WriteLine($"My name is {name} and my lastname is {lastName}");
+
+//------------------VARIABLES----------------------------------
+
+//Varaibles init
+int yearOfBirth;
+string firstName;
+
+//Variables init and value assign
+string lastName2 = "Pavardenis";
+
+//decimal suffix M
+decimal money = 1999.9m;
+float num = 1999.9f;
+
+DateTime lastPayDay = DateTime.Now;
+DateTime date = new DateTime(2022, 01, 01, 16, 15, 20);
+DateTime decreasedDate = date.AddDays(-5);
+
+//https://www.c-sharpcorner.com/blogs/date-and-time-format-in-c-sharp-programming1
+string formattedDate = lastPayDay.ToString("dddd, dd MMMM yyyy");
+
+char y = "Pavardenis"[0];
+int ilgis = "Pavardenis".Length;
+
+//------------LOGIC OPERATORS------------------------------------------
+bool A = false;
+bool B = false;
+
+bool A_OR_B = A || B; //=> FALSE
+bool A_AND_B = A && B; //=> FALSE
+bool A_NOT = !A; // => TRUE
+
+
+bool A1 = false;
+bool B1 = true;
+
+bool A1_OR_B1 = A1 || B1; //=> TRUE
+bool A1_AND_B1 = A1 && B1; //=> FALSE
+bool B1_NOT = !B1; //=> FALSE
+
+bool A3 = !(!A1 && (!(A || A1) && (A && B1)));
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Topic_4_Cycles_Collections/Program.cs
@@ -0,0 +1,61 @@
+﻿// 5.
+int[] numArray = new int[10];
+int[] numArray2 = new int[10];
+
+Random random = new Random();
+
+for (int i = 0; i < numArray.Length; i++) 
+{
+    numArray[i] = random.Next(1, 100);
+    numArray2[i] = random.Next(1, 100);
+}
+
+int[] concatedArray = new int[numArray.Length + numArray2.Length];
+
+numArray.CopyTo(concatedArray, 0);
+numArray2.CopyTo(concatedArray, numArray.Length);
+
+foreach (int item in concatedArray)
+{
+    Console.WriteLine(item);
+}
+
+for (int i = 0; i < concatedArray.Length; i++)
+{
+    for (int j = i + 1; j < concatedArray.Length; j++)
+    {
+        if (concatedArray[i] > concatedArray[j]) 
+        {
+            int temp = concatedArray[i];
+            concatedArray[i] = concatedArray[j];
+            concatedArray[j] = temp;
+        }
+    }
+}
+
+foreach (int item in concatedArray)
+{
+    Console.WriteLine(item);
+}
+
+//List<T>
+List<string> stringList = new List<string>();
+var stringLists = new List<List<string>>();
+var intList = new List<int>();
+List<bool> boolList = new List<bool>();
+
+List<object> objectList = new List<object>();
+
+stringList.Add("stringas");
+intList.Add(1);
+
+objectList.Add(1);
+objectList.Add(true);
+objectList.Add("stringas");
+//intList.Add("stringas"); => not valid
+
+
+
+
+
+
