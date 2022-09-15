9400b6fffabd0a773e5f7d0928c920d3beecc2a1
Redas Rimkus <redas.rimkus93@gmail.com>
2022-09-15 09:55:09 +0300

    Added methods topic

---
+++ Topic_5_Methods/Program.cs
@@ -0,0 +1,26 @@
+﻿class Methods
+{
+    // Method with no return
+    void PrintSum(int digit1, int digit2)
+    {
+        Console.WriteLine($"Sum is {digit1 + digit2}");
+    }
+
+    // Method with return type
+    int CountSum(int digit1, int digit2)
+    {
+        return digit1 + digit2;
+    }
+
+    // Method overloading - same method names different parameters
+
+    int Sum(int digit1, int digit2)
+    {
+        return digit1 + digit2;
+    }
+
+    int Sum(int digit1, int digit2, int digit3)
+    {
+        return digit1 + digit2 + digit3;
+    }
+}
