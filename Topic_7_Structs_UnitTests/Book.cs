246f2ac71b6eee29ee9a5e07f8f090df09655d66
Redas Rimkus <redas.rimkus93@gmail.com>
2022-09-26 21:52:38 +0300

    Added topic about the struct

---
+++ Topic_7_Structs_UnitTests/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksLibrary
+{
+    public struct Book
+    {
+        public string title = "Bible"; // Default value
+        public string author;
+        public int id;
+        public int pages;
+
+        public Book(string title, string author, int id, int pages)
+        {
+            this.title = title;
+            this.author = author;
+            this.id = id;
+            this.pages = pages;
+        }
+
+        public void PrintTitle()
+        {
+            Console.WriteLine(title);
+        }
+    }
+}
---
+++ Topic_7_Structs_UnitTests/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BooksLibrary;
+
+Book harryPotter;
+
+harryPotter.author = "Rowling";
+harryPotter.title = "Harry potter";
+harryPotter.id = 1;
+harryPotter.pages = 600;
+
+Book lordOfTheRings = new Book("Lord of the Rings", "Tolkien", 2, 540);
+
+Console.WriteLine($"An author of {harryPotter.title} is {harryPotter.author}. And an author of {lordOfTheRings.title} is {lordOfTheRings.author}");
+
+lordOfTheRings.PrintTitle();
+
